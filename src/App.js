b684fa8e1479e41bb4d2e86ef1d3f3b0ab6553b8
Carlos Aguilar <carlos.aguilar@scgts.com>
2021-10-24 23:37:56 -0500

    useState & useEffect hooks

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 
 const App = () => {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
+  useEffect(()=>{
+    navigator.geolocation.getCurrentPosition(position=>{
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    })
+
+    console.log("Latitude is:", lat);
+    console.log("Longitude is:", long);
+  }, [lat, long])
+
   return (
   <div className="App">
     <h1>Esto es un H1</h1>
