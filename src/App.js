f0b5f87360c87aec92bd59481b52be8c238828a6
Carlos Aguilar <carlos.aguilar@scgts.com>
2021-10-24 23:52:41 -0500

    fetch data function

--- src/App.js
+++ src/App.js
@@ -4,21 +4,32 @@ import React, { useState, useEffect } from "react";
 const App = () => {
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
 
-  useEffect(()=>{
-    navigator.geolocation.getCurrentPosition(position=>{
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    })
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
 
-    console.log("Latitude is:", lat);
-    console.log("Longitude is:", long);
-  }, [lat, long])
+      await fetch(
+        `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+          console.log(result);
+        });
+    };
+    fetchData();
+  }, [lat, long]);
 
   return (
-  <div className="App">
-    <h1>Esto es un H1</h1>
-  </div>)
+    <div className="App">
+      <h1>Esto es un H1</h1>
+    </div>
+  );
 };
 
 export default App;
